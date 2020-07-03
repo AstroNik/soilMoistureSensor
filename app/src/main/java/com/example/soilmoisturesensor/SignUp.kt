77bdd5649c64d5c631f2476293a55000f82fa939
Nikhil <nikhil.kapadia.97@gmail.com>
2020-07-02 23:53:29 -0400

    Made small changes to signup to add sending user data to our service. Need to implement sending data still

--- app/src/main/java/com/example/soilmoisturesensor/SignUp.kt
+++ app/src/main/java/com/example/soilmoisturesensor/SignUp.kt
@@ -1,14 +1,15 @@
 package com.example.soilmoisturesensor
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUp : AppCompatActivity() {
 
@@ -20,37 +21,53 @@ class SignUp : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
 
-        ok.setOnClickListener(View.OnClickListener { view-> register()
+        ok.setOnClickListener(View.OnClickListener { view ->
+            register()
 
             //Save the data in the database, display a message that account has been created
         })
 
-        cancel.setOnClickListener{
+        cancel.setOnClickListener {
 
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
-    private fun register(){
+    private fun register() {
 
         val mName = fullName.text.toString()
         val mEmail = Email.text.toString()
         val mPwd = password1.text.toString()
         val mPwd1 = password2.text.toString()
 
-        if(!mName.isEmpty() && !mEmail.isEmpty() && !mPwd.isEmpty()){
+        if (!mName.isEmpty() && !mEmail.isEmpty() && !mPwd.isEmpty()) {
 
-            if(mPwd.equals(mPwd1)) {
+            if (mPwd.equals(mPwd1)) {
 
                 mAuth.createUserWithEmailAndPassword(mEmail, mPwd)
                     .addOnCompleteListener(this, OnCompleteListener { task ->
 
                         if (task.isSuccessful) {
-                            //val user = mAuth.currentUser
-                            //val uid = user!!.uid
-                            // mDB.child(uid).child("Names").setValue(mName)
 
-                            /*****-------- DB Code -------------*****/
+                            val mUser = FirebaseAuth.getInstance().currentUser
+                            mUser!!.getIdToken(true)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        val idToken = task.result!!.token
+                                        //TODO: Send User's Name/LastName Email and idToken
+                                        val uid = mUser.uid
+                                        val email = mEmail //sub this out to just mEmail when sending
+                                        val firstName = mName //split this into first and last name in the form
+                                        val lastName = mName
+                                        val token = idToken // can simply just pass
+                                        //THE ENDPOINT IS https://www.ecoders.ca/addUser
+
+
+
+                                    } else {
+                                        Log.d("ERROR Creating Token",task.exception.toString());
+                                    }
+                                }
 
                             startActivity(Intent(this, MainActivity::class.java))
 
@@ -60,13 +77,11 @@ class SignUp : AppCompatActivity() {
 
                         }
                     })
-            }
-            else{
+            } else {
 
                 Toast.makeText(this, "Password doesnot match", Toast.LENGTH_LONG).show()
             }
-        }
-        else{
+        } else {
 
             Toast.makeText(this, "Please fill values", Toast.LENGTH_SHORT).show()
         }
